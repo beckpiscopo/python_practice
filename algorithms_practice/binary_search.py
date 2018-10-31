0785297b8b1117f9ecd37b14cf9486f642e2f424
brittbec <hello@brittbec.com>
2018-10-31 14:52:46 -0500

    finished first binary search

--- algorithms_practice/binary_search.py
+++ algorithms_practice/binary_search.py
@@ -1,13 +1,27 @@
 def binary_search(list, target): 
-    first = 0
-    last = len(list) - 1
+    low = 0
+    high = len(list) - 1
 
-    while first <= last: 
-        midpoint = (first + last) // 2
+    while low <= high: 
+        midpoint = (low + high) // 2
 
         if list[midpoint] == target: 
             return midpoint
-        else list[midpoint] < target: 
-            first = midpoint + 1 
+        elif list[midpoint] < target:        # if midpoint is less than the target, we remove the first half of list from search
+            low = midpoint + 1               # the new value for the low is midpoint + 1 
         else:
-            less = midpoint - 1
+            high = midpoint - 1              # if midpoint is higher than the target, we remove the last half of list from search 
+    return None
+
+def verify(index): 
+    if index is not None: 
+        print('The target is at index: {}'.format(index))
+    else: 
+        print("Target not found.")
+
+numbers = range(1,10)
+
+result = binary_search(numbers, 12)
+verify(result)
+result = binary_search(numbers, 3)
+verify(result)
