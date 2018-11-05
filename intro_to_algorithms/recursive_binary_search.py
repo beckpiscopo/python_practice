97fbee5a23992c2885146bb5ec489c3a9a3b9247
brittbec <hello@brittbec.com>
2018-11-05 16:41:36 -0600

    reviewed recursive binary search, added comments

--- intro_to_algorithms/recursive_binary_search.py
+++ intro_to_algorithms/recursive_binary_search.py
@@ -2,15 +2,17 @@ def recursive_binary_search(list, target):
     if len(list) == 0: 
         return False
     else: 
-        midpoint = len(list) // 2
+        midpoint = len(list) // 2  # find midpoint 
 
         if list[midpoint] == target: 
             return True
         else: 
-            if list[midpoint] < target: 
-                return recursive_binary_search(list[midpoint + 1:], target)
+            if list[midpoint] < target:           # is midpoint less than target? 
+                # if midpoint is less than target, return last half of the list
+                return recursive_binary_search(list[midpoint + 1:], target)    
             else: 
-                return recursive_binary_search(list[:midpoint], target)
+                # if midpoint is greater than target, return first half of list
+                return recursive_binary_search(list[:midpoint], target)        
 
 def verify(result): 
     print("Target found: {}".format(result))
