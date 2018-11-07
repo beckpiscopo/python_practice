831373b31389961bd928991a77f9541f1cdcfa99
brittbec <hello@brittbec.com>
2018-11-06 19:02:37 -0600

    Learning about merge sort algorithm

---
+++ intro_to_data_structures/merge_sort.py
@@ -0,0 +1,8 @@
+def merge_sort(list): 
+    """
+    Sorts list in ascending order
+    Returns a new sorted list
+
+    Divide: Finds the midpoint of list and divide into sublists
+    Conquer: Recursively sort the sublists created in the previous step
+    Conquer: Merge the sorted sublists created in the previous step
