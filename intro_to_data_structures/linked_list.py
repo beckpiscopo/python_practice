c1d0daa461f083a887641cef4eb1b298229e41b2
brittbec <hello@brittbec.com>
2018-11-06 18:08:06 -0600

    added remove() function to linked list

--- intro_to_data_structures/linked_list.py
+++ intro_to_data_structures/linked_list.py
@@ -88,6 +88,27 @@ class LinkedList:
             prev_node.next_node = new
             new.next_node = next_node
 
+    def remove(self, key): 
+        """ 
+        Searches for and removes node containing data that matches key 
+        Returns the node or none if it doesn't exist. 
+        Takes O(n) runtime
+        """
+        current = self.head
+        previous = None
+        found = False 
+        while current and not found: 
+            if current.data == key and current is self.head: 
+                found = True
+                self.head = current.next_node
+            elif current.data == key: 
+                found = True
+                previous.next_node = current.next_node
+            else: 
+                previous = current
+                current = current.next_node
+        return current
+
 
     def __repr__(self): 
         """
