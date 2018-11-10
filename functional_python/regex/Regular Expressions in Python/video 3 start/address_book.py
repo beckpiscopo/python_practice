eb8ddc452843b0d5308dcb136134585de0ca8e9a
brittbec <hello@brittbec.com>
2018-11-10 17:04:50 -0600

    learning how to remove files-- hopefully

--- functional_python/regex/Regular Expressions in Python/video 3 start/address_book.py
+++
@@ -1,9 +0,0 @@
-import re
-
-names_file = open("names.txt", encoding="utf-8")
-data = names_file.read()
-names_file.close()
-
-#print(re.match(r'Love', data))
-#print(re.search(r'Kenneth', data))
-print(re.search(r'\(\d\d\d\) \d\d\d-\d\d\d\d', data))
--- functional_python/treehouse_resources/S1V4/functions.py
+++
@@ -1,8 +0,0 @@
-def log_and_run(func):
-    print("I just got {}".format(func.__name__))
-    return func()
-
-
-def log_and_return(func):
-    print("I just got {}".format(func.__name__))
-    return func
