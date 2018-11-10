07faeb7117f06639cd74fba6cd680b09b4341d68
brittbec <hello@brittbec.com>
2018-11-09 20:43:58 -0600

    learning Regex and Functional Python

---
+++ functional_python/treehouse_resources/S1V3/stock.py
@@ -0,0 +1,37 @@
+import json
+from operator import attrgetter, itemgetter
+
+
+class Book:
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return str(self)
+
+
+def get_books(filename, raw=False):
+    try:
+        data = json.load(open(filename))
+    except FileNotFoundError:
+        return []
+    else:
+        if raw:
+            return data['books']
+        return [Book(**book) for book in data['books']]
+
+
+BOOKS = get_books('books.json')
+RAW_BOOKS = get_books('books.json', raw=True)
+
+# pub_sort = sorted(RAW_BOOKS, key=itemgetter('publish_date'))
+# print(pub_sort[0]['publish_date'], pub_sort[-1]['publish_date'])
+
+
+pages_sort = sorted(BOOKS, key=attrgetter('number_of_pages'))
+print(pages_sort[0].number_of_pages,
+      pages_sort[-1].number_of_pages)
