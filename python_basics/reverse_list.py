698475a857a66b26e052729679829a6f6fe68e4d
brittbec <hello@brittbec.com>
2018-11-10 23:26:20 -0600

    practicing list reversal, filter() function

--- python_basics/reverse_list.py
+++ python_basics/reverse_list.py
@@ -1,15 +1,30 @@
 """here's how to use the map() function to find the reversed version of items in a list"""
 
-backwards = [
-    'tac',
-    'rewolf',
-    'noisivelet',
-    [5, 4, 3, 2, 1]
+# backwards = [
+#    'tac',
+#    'rewolf',
+#    'noisivelet',
+#    [5, 4, 3, 2, 1]
+# ]
+
+
+# def reverse(item):
+#    return item[::-1]
+
+
+# sdrawkcab = print(list(map(reverse, backwards)))
+
+
+dimensions = [
+    (5, 5),
+    (10, 10),
+    (6, 6),
+    (7, 7)
 ]
 
 
-def reverse(item):
-    return item[::-1]
+def area(m):
+    return m[0] * m[1]
 
 
-sdrawkcab = print(list(map(reverse, backwards)))
+print(list(map(area, dimensions)))
