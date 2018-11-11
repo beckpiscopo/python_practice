070f6223dd836d65f84f15ee07b1a2bb22fe9b89
brittbec <hello@brittbec.com>
2018-11-11 15:21:53 -0600

    Learning about chaining functions; first time using any() function

--- functional_python/functional_python_treehouse/treehouse_resources/S2V4/stock.py
+++ functional_python/functional_python_treehouse/treehouse_resources/S2V4/stock.py
@@ -2,6 +2,7 @@ from copy import copy
 import json
 from operator import attrgetter, itemgetter
 
+
 class Book:
     def __init__(self, **kwargs):
         for k, v in kwargs.items():
@@ -9,11 +10,11 @@ class Book:
 
     def __str__(self):
         return self.title
-    
+
     def __repr__(self):
         return str(self)
-    
-    
+
+
 def get_books(filename, raw=False):
     try:
         data = json.load(open(filename))
@@ -23,7 +24,8 @@ def get_books(filename, raw=False):
         if raw:
             return data['books']
         return [Book(**book) for book in data['books']]
-    
+
+
 BOOKS = get_books('books.json')
 RAW_BOOKS = get_books('books.json', raw=True)
 
@@ -34,10 +36,12 @@ RAW_BOOKS = get_books('books.json', raw=True)
 # print(pages_sort[0].number_of_pages, pages_sort[-1].number_of_pages)
 
 #important_list = [5, 3, 1, 2, 4]
-#important_list.sort()  # Bad idea, sorts list in place
+# important_list.sort()  # Bad idea, sorts list in place
 # sorted(important_list)  # Sorts a copy of the list
 
 ### MAP ###
+
+
 def sales_price(book):
     """Apply a 20% discount to the book's price"""
     book = copy(book)
@@ -48,11 +52,39 @@ def sales_price(book):
 # sales_books2 = [sales_price(book) for book in BOOKS]
 
 ### FILTER ###
+
+
 def is_long_book(book):
     """Does a book have 600+ pages?"""
     return book.number_of_pages >= 600
 
+
 long_books = list(filter(is_long_book, BOOKS))
 long_books2 = [book for book in BOOKS if book.number_of_pages >= 600]
-print(len(BOOKS))
-print(len(long_books2))
+
+### Chaining ###
+
+
+def has_roland(book):
+    return any(["Roland" in subject for subject in book.subjects])
+
+
+def titlecase(book):
+    book = copy(book)
+    book.title = book.title.title()
+    return book
+
+
+# Map titlecase to whatever comes back from filter()
+# print(list(map(titlecase, filter(has_roland, BOOKS))))
+
+def is_good_deal(book):
+    return book.price <= 5
+
+
+cheap_books = sorted(
+    filter(is_good_deal, map(sales_price, BOOKS)),
+    key=attrgetter('price')
+)
+
+print(cheap_books[0].price)
