52867abfe332f73a676ea5cc9f23c0e35c060806
Brittany Beckett-Harrison <brittanybeckett-harrison@B-Beckett-Harrison.local>
2018-11-23 16:59:40 -0600

    finished linked list merge sort algorithm fromTreehouse's Intro to Data Structures course

--- intro_to_data_structures/linked_list_merge_sort.py
+++ intro_to_data_structures/linked_list_merge_sort.py
@@ -1,7 +1,7 @@
 from linked_list import LinkedList
 
 
-def merge_sort():
+def merge_sort(linked_list):
     """
    Sorts a linked list in ascending order
    - Recursively divide a linked lists into sublists containing a single node
@@ -9,19 +9,19 @@ def merge_sort():
    - Returns one sorted list
    """
 
-       if linked_list.size == 1:
-            return linked_list
-        elif linked_list.head is None
-          return linked_list
+    if linked_list.size() == 1:
+        return linked_list
+    elif linked_list.head is None:
+        return linked_list
 
-        left_half, right_half = split(linked_list)
-        left = merge_sort(left_half)
-        right = merge_sort(right_half)
+    left_half, right_half = split(linked_list)
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
 
-        return merge(left, right)
+    return merge(left, right)
 
 
-def split():
+def split(linked_list):
     """
     Divide unsorted lists at midpoint into two lists
     """
@@ -32,7 +32,7 @@ def split():
 
         return left_half, right_half
     else:
-        size = linked_list.size
+        size = linked_list.size()
         mid = size // 2
 
         mid_node = linked_list.node_at_index(mid-1)
@@ -42,7 +42,77 @@ def split():
         right_half.head = mid_node.next_node
         mid_node.next_node = None
 
-        return left_half, right_half
+
+def merge(left, right):
+    """
+    Merges two linked lists, sorting by data in the nodes
+    Returns a new, merged list
+    """
+
+    # Create a new linked list that contains nodes from
+    # merging left and right
+
+    merged = LinkedList()
+
+    # Add a fake head that is discarded later
+    merged.add(0)
+
+    # Set current to the head of the linked list
+    current = merged.head
+
+    # Obtain head nodes for left and right linked lists
+    left_head = left.head
+    right_head = right.head
+
+    # Iterate over left and right until we reach the tail node of either
+    while left_head or right_head:
+        # If the head node of left node is None, we're past the tail
+        # Add the node from right to merged linked list
+        if left_head is None:
+            current.next_node = right_head
+            # Call next on right to set loop condition to False
+            right_head = right_head.next_node
+        # If the head node of right is None, we're past the tail
+        # Add the tail node from left to merged linked list
+        elif right_head is None:
+            current.next_node = left_head
+            # Call next on left to set loop condition to False
+            left_head = left_head.next_node
+        else:
+            # Not at either tail node
+            # Obtain node data to perform comparison operations
+            left_data = left_head.data
+            right_data = right_head.data
+            # If data on left is less than right, set current to left node
+            if left_data < right_data:
+                current.next_node = left_head
+                # Move left head to next node
+                left_head = left_head.next_node
+
+            # If data on left is greater than right, set current to right node
+            else:
+                current.next_node = right_head
+                # Move right head to next node
+                right_head = right_head.next_node
+        # Move current to next node
+        current = current.next_node
+    # Discard fake head and set first merged node as head
+    head = merged.head.next_node
+    merged.head = head
+
+    return merged
+
+# Verify that algorithm works
 
 
-# def merge():
+l = LinkedList()
+l.add(20)
+l.add(32)
+l.add(10)
+l.add(3)
+l.add(56)
+
+print(l)
+
+sorted_linked_list = merge_sort(l)
+print(sorted_linked_list)
