47896a72b7ec2775893f461d2049ca0df3daf08f
Brittany Beckett-Harrison <brittanybeckett-harrison@B-Beckett-Harrison.local>
2018-12-12 18:35:13 -0600

    updated comments

--- python_basics/indexing_lists.py
+++ python_basics/indexing_lists.py
@@ -1,7 +1,7 @@
-# TODO Create an empty list to maintain the player names
+# Create an empty list to maintain the player names
 roster = []
 
-# TODO Ask the user if they'd like to add players to the list.
+# Ask the user if they'd like to add players to the list.
 # If the user answers "Yes", let them type in a name and add it to the list.
 # If the user answers "No", print out the team 'roster'
 
@@ -12,10 +12,10 @@ while add_players.lower() == "yes":
     add_players = input(
         "Would you like to add another player to the list? (y/n)  ")
 
-# TODO print the number of players on the team
+# print the number of players on the team
 print("There are {} players on the team.".format(len(roster)))
 
-# TODO Print the player number and the player name
+# Print the player number and the player name
 # The player number should start at the number one
 player_number = 1
 for player in roster:
@@ -23,12 +23,12 @@ for player in roster:
     player_number += 1
 
 
-# TODO Select a goalkeeper from the above roster
+# Select a goalkeeper from the above roster
 keeper = input(
     "Please select a goal keeper by selecting player number. (1 - {})  ".format(len(roster)))
 keeper = int(keeper)
 
-# TODO Print the goal keeper's name
+# Print the goal keeper's name
 # Remember that lists use a zero based index
 
 print("The goalkeeper's names is {}".format(roster[keeper - 1]))
